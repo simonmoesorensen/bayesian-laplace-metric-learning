3f7bc135f3c3e456c3df7e54869d3a58f3e0c7e4
johndoe133 <erickastljensen@gmail.com>
2022-06-28 16:31:54 +0200

    updated instructions for making n digit mnist

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 | `hedged_instance_embedding`     | [MODELING UNCERTAINTY WITH HEDGED INSTANCE EMBEDDING](https://arxiv.org/pdf/1810.00319.pdf)                | [RRoundTable](https://github.com/RRoundTable/hedged_instance_embedding)                                        |
 | `probabilistic_face_embedding`  | [Probabilistic Face Embeddings](https://arxiv.org/pdf/1904.09658.pdf)                                      | [seasonSH](https://github.com/seasonSH/Probabilistic-Face-Embeddings)                                          |
 | `unsupervised_visual_retrieval` | [Unsupervised Data Uncertainty Learning in Visual Retrieval Systems](https://arxiv.org/pdf/1902.02586.pdf) |                                                                                                                |
+
+
+To make n-digit MNIST dataset, you need to have a data directory, and then run `download.sh` to get the mnist dataset. Make sure you also have downloaded the `standard_datasets` directory from https://github.com/google/n-digit-mnist. Then, run `n_digit_mnist.py` with `python n_digit_mnist.py --num_digits 2 --domain_gap instance --use_standard_dataset` to make the 2 digit mnist dataset in `data/dataset_mnist_2_instance` directory. 
--- src/baselines/hedged_instance_embedding/model.py
+++ src/baselines/hedged_instance_embedding/model.py
@@ -1,9 +1,58 @@
+# torch stuff
 import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.utils.data
+from torch.autograd import Variable
+
+
+# numpy
 import numpy as np
+
+# plotting
+import matplotlib.pyplot as plt
+
+BATCH_SIZE=200
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-a = np.load('data/dataset_mnist_2_instance/test.npz')
 
-for file in a.files:
-    print(file)
-    print(a[file].shape)
+# load in 2 digit mnist dataset
+test = np.load('data/dataset_mnist_2_instance/test.npz')
+train = np.load('data/dataset_mnist_2_instance/train.npz')
+
+x_test = torch.flatten(torch.from_numpy(test['images']),start_dim=1).view(-1, 1,28,56).float()
+y_test = torch.from_numpy(test['labels'])
+
+x_train = torch.flatten(torch.from_numpy(train['images']),start_dim=1).view(-1, 1,28,56).float()
+y_train = torch.from_numpy(train['labels'])
+
+train = torch.utils.data.TensorDataset(x_train,y_train)
+test = torch.utils.data.TensorDataset(x_test,y_test)
+
+train_loader = torch.utils.data.DataLoader(train, batch_size = BATCH_SIZE, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = BATCH_SIZE, shuffle = False)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
+        self.fc1 = nn.Linear(3*3*64, 256)
+        self.fc2 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        #x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(F.max_pool2d(self.conv3(x),2))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = x.view(-1,3*3*64 )
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+ 
+cnn = CNN()
+
