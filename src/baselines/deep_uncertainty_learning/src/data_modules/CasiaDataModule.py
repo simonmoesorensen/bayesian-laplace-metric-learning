6e538dda6be75531db0dac4e2861c84a4d6b4330
simonmoesorensen <moe.simon@gmail.com>
2022-07-08 14:58:15 +0200

    ✨feat(casia-data-module): Add implementation for CasiaDataModule which inherits the BaseDataModule interface

---
+++ src/baselines/deep_uncertainty_learning/src/data_modules/CasiaDataModule.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from torchvision import transforms
+import torchvision.datasets as d
+from torch.utils.data import Subset, random_split
+import zipfile
+
+from tqdm import tqdm
+from gdrive import download_file_from_google_drive
+
+from BaseDataModule import BaseDataModule
+
+
+class CasiaDataset(d.ImageFolder):
+    def __init__(self, root, transform=None, target_transform=None):
+        super().__init__(root, transform, target_transform)
+
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+
+
+class CasiaDataModule(BaseDataModule):
+    def __init__(self, data_dir, batch_size, num_workers):
+        super().__init__(CasiaDataset, data_dir, batch_size, num_workers)
+
+        self.name = "Cassia"
+        self.n_classes = 10575
+
+        self.img_path = self.data_dir / "CASIA-WebFace"
+
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                transforms.Resize((64, 64)),
+            ]
+        )
+
+    def prepare_data(self):
+        file_id = "1Of_EVz-yHV7QVWQGihYfvtny9Ne8qXVz"
+        file_path = self.data_dir / "casia.zip"
+
+        if zipfile.is_zipfile(file_path):
+            print("Files already downloaded")
+        else:
+            print(f"Downloading casia file to {file_path}")
+            download_file_from_google_drive(file_id, file_path)
+
+
+        if self.img_path.exists():
+            print(f"Files already extracted in {self.img_path}")
+        else:
+            print(f"Extracting {file_path} to {self.img_path}")
+            with zipfile.ZipFile(file_path, "r") as zf:
+                for member in tqdm(zf.infolist(), desc="Extracting "):
+                    try:
+                        zf.extract(member, self.img_path)
+                    except zipfile.error as e:
+                        print(e)
+                        pass
+
+    def setup(self, stage=None, val_split=0.2, test_split=0.2, shuffle=False):
+        assert self.transform, "transform must be set before setup()"
+
+        dataset_full = self.cls(self.img_path, transform=self.transform)
+        size = len(dataset_full)
+
+        def get_split_size(frac):
+            return int(size // (1 / frac))
+
+        n_train = get_split_size(1 - val_split - test_split)
+        n_val = get_split_size(1 - val_split)
+        n_test = get_split_size(1 - test_split)
+
+        if shuffle:
+            self.dataset_train, self.dataset_val, self.dataset_test = random_split(
+                dataset_full, [n_train, n_val, n_test]
+            )
+        else:
+            self.dataset_train = Subset(dataset_full, range(0, n_train))
+            self.dataset_val = Subset(dataset_full, range(n_train, n_val))
+            self.dataset_test = Subset(dataset_full, range(n_val, n_test))
+
+
+if __name__ == "__main__":
+    data_dir = Path("/work3/s174420/datasets")
+
+    data = CasiaDataModule(data_dir, batch_size=64, num_workers=12)
+    data.prepare_data()
+
+    data.setup()
+
+    print(next(iter(data.train_dataloader())))
