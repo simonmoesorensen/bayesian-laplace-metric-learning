aa1e66272e9f17952cd04080dcd3451639a5e140
simonmoesorensen <moe.simon@gmail.com>
2022-07-21 12:09:05 +0200

    ⚙️ refactor(hib-train): refactor loss step to log losses inside func instead of returning

--- src/lightning_modules/HIBLightningModule.py
+++ src/lightning_modules/HIBLightningModule.py
@@ -57,7 +57,6 @@ class HIBLightningModule(BaseLightningModule):
 
         self.loss_fn.apply(self.loss_fn.weight_clipper)
 
-
     def epoch_start(self):
         self.metrics.reset(
             ["train_loss", "train_loss_kl", "train_accuracy", "train_map_r"]
@@ -70,36 +69,40 @@ class HIBLightningModule(BaseLightningModule):
         with self.autocast():
             # Create sample distribution
             pdist = tdist.Normal(mu, std + 1e-20)
-            
-            # Monte Carlo K times sampling, reparameterization trick in order 
+
+            # Monte Carlo K times sampling, reparameterization trick in order
             # to do backprop
             # [K_samples, batch_size, embedding_space]
             samples = self.to_device(pdist.rsample([self.K]))
 
             pair_indices = self.miner(samples[0], y)
 
-            # Repeat interleave tensor so something like [[1,2],[3,4],[4,5]] becomes 
+            # Repeat interleave tensor so something like [[1,2],[3,4],[4,5]] becomes
             # [[1,2],[1,2],[3,4],[3,4],[4,5],[4,5]]
             k1_samples = samples.repeat_interleave(self.K**2, dim=0)
 
             # Repeat tensor so something like [[1,2],[3,4],[4,5]] becomes
             # [[1,2],[3,4],[4,5],[1,2],[3,4],[4,5]]
             k2_samples = samples.repeat(self.K**2, 1, 1)
-                
+
             # Convert 3D to 2D, we use self.K**3 because we have K_samples, repeated K**2 times
             # Concatenates all K samples into one large [batch_size * K, embedding_space] tensor
-            k1_samples = k1_samples.view(self.K**3 * mu.shape[0], self.args.embedding_size)
-            k2_samples = k2_samples.view(self.K**3 * mu.shape[0], self.args.embedding_size)
-            
+            k1_samples = k1_samples.view(
+                self.K**3 * mu.shape[0], self.args.embedding_size
+            )
+            k2_samples = k2_samples.view(
+                self.K**3 * mu.shape[0], self.args.embedding_size
+            )
+
             # Repeat target to match the shape of the samples [batch_size * K, embedding_space]
             y_k1 = y.repeat_interleave(self.K**3, dim=0)
             y_k2 = y.repeat(self.K**3, 1).view(-1)
 
             # Scale the indices to match the shape of the samples
             step = self.to_device(torch.arange(0, self.K)) * self.batch_size
-            scale_indices = lambda x: (
-                    x.repeat(self.K, 1).T + step
-                ).T.view(-1)
+
+            def scale_indices(x):
+                return (x.repeat(self.K, 1).T + step).T.view(-1)
 
             scaled_indices = []
             for val in pair_indices:
@@ -116,10 +119,13 @@ class HIBLightningModule(BaseLightningModule):
 
             # Compare to unit gaussian r(z) ~ N(0, I)
             zdist = tdist.Normal(torch.zeros_like(mu), torch.ones_like(std))
-            
+
             loss_kl = tdist.kl_divergence(pdist, zdist).sum() / mu.shape[0]
 
             loss = loss_soft_contrastive + self.args.kl_scale * loss_kl
+
+            self.metrics.update(f"{step}_loss", loss_soft_contrastive.item())
+            self.metrics.update(f"{step}_loss_kl", loss_kl.item())
 
             return samples, loss, loss_soft_contrastive, loss_kl
 
@@ -127,10 +133,7 @@ class HIBLightningModule(BaseLightningModule):
         # Pass images through the model
         mu, std = self.forward(X)
 
-        samples, loss, loss_soft_contrastive, loss_kl = self.loss_step(mu, std, y)       
-
-        self.metrics.update("train_loss", loss_soft_contrastive.item())
-        self.metrics.update("train_loss_kl", loss_kl.item())
+        samples, loss = self.loss_step(mu, std, y, step="train")
 
         return samples[0], loss
 
@@ -140,13 +143,10 @@ class HIBLightningModule(BaseLightningModule):
     def val_step(self, X, y):
         mu, std = self.forward(X)
 
-        samples, _, loss_soft_contrastive, loss_kl = self.loss_step(mu, std, y)      
-
-        self.metrics.update("val_loss", loss_soft_contrastive.item())
-        self.metrics.update("val_loss_kl", loss_kl.item())
+        samples, _ = self.loss_step(mu, std, y, step="val")
 
         return mu, std, samples[0]
-    
+
     def val_end(self):
         self.log(["val_loss", "val_loss_kl", "val_accuracy", "val_map_r"])
 
@@ -158,10 +158,10 @@ class HIBLightningModule(BaseLightningModule):
             "Validation Accuracy {acc.val:.4f} ({acc.avg:.4f})\t"
             "Validation MAP@r {map_r.val:.4f} ({map_r.avg:.4f}))".format(
                 time.asctime(time.localtime(time.time())),
-                loss=self.metrics.get('val_loss'),
-                loss_KL=self.metrics.get('val_loss_kl'),
-                acc=self.metrics.get('val_accuracy'),
-                map_r=self.metrics.get('val_map_r'),
+                loss=self.metrics.get("val_loss"),
+                loss_KL=self.metrics.get("val_loss_kl"),
+                acc=self.metrics.get("val_accuracy"),
+                map_r=self.metrics.get("val_map_r"),
             ),
             flush=True,
         )
@@ -186,7 +186,7 @@ class HIBLightningModule(BaseLightningModule):
             "Training Loss {loss.val:.4f} ({loss.avg:.4f})\t"
             "Training Loss_KL {loss_KL.val:.4f} ({loss_KL.avg:.4f})\t"
             "Training Accuracy {acc.val:.4f} ({acc.avg:.4f})\t"
-            "Training MAP@r {map_r.val:.4f} ({map_r.avg:.4f})"
+            "Training MAP@r {map_r.val:.4f} ({map_r.avg:.4f})\t"
             "Lr {lr:.4f}".format(
                 epoch + 1,
                 self.args.num_epoch,
