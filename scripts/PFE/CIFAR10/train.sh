c2aba28254afe0fd27b5dde2e9f66bb9cc90ace4
simonmoesorensen <moe.simon@gmail.com>
2022-07-26 14:06:35 +0200

    ✨ feat(scripts): Update scripts

--- scripts/PFE/CIFAR10/train.sh
+++ scripts/PFE/CIFAR10/train.sh
@@ -4,10 +4,10 @@ echo "Waiting for debugger to attach..."
 python3 -m debugpy --listen 10.66.20.9:1332 ./src/baselines/PFE/train.py \
     --dataset CIFAR10 \
     --name CIFAR10 \
-    --batch_size 128 \
+    --batch_size 256 \
     --embedding_size 512 \
     --num_epoch 20 \
-    --save_freq 25 \
+    --save_freq 2 \
     --gpu_id 0 1\
     --num_workers 8 \
     --shuffle \
