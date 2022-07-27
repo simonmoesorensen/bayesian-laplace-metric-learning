e3b91851c0a5745d9ffe9c47da5aef0b559dadea
simonmoesorensen <moe.simon@gmail.com>
2022-07-27 15:12:48 +0200

    ✨ feat(scripts): Update mnist script for hib

--- scripts/HIB/MNIST/train.sh
+++ scripts/HIB/MNIST/train.sh
@@ -1,16 +1,17 @@
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=1
 
 echo "Waiting for debugger to attach..."
-python3 -m debugpy --listen 10.66.20.9:1331 ./src/baselines/HIB/train.py \
+python3 -m debugpy --listen 10.66.12.19:1332 ./src/baselines/HIB/train.py \
     --dataset MNIST \
     --name MNIST \
-    --batch_size 64 \
+    --batch_size 128 \
     --K 2 \
-    --embedding_size 128 \
+    --embedding_size 512 \
     --num_epoch 6 \
-    --save_freq 2 \
-    --gpu_id 0 1\
+    --save_freq 1 \
+    --gpu_id 0\
     --num_workers 8 \
     --shuffle \
-    --kl_scale 0.00015 \
-    --to_visualize
+    --kl_scale 1e-4 \
+    --to_visualize \
+    --lr 1e-4
