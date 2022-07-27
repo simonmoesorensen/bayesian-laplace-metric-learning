7d0d305f7bd92e10e77ff1430107385ed3a9548b
Silas Brack <s174433@student.dtu.dk>
2022-07-27 15:28:15 +0200

    Added mnist and fashionmnist.

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,3 +2,5 @@ from src.data.cifar10 import CIFAR10DataModule
 from src.data.cifar100 import CIFAR100DataModule
 from src.data.noise import NoiseDataModule
 from src.data.svhn import SVHNDataModule
+from src.data.mnist import MNISTDataModule
+from src.data.fashion_mnist import FashionMNISTDataModule
