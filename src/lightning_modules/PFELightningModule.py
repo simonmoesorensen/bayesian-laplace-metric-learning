90eca41c15ad2c679f04008f7bfa26ea7b4a5210
johndoe133 <erickastljensen@gmail.com>
2022-08-04 08:47:25 +0200

    Added pdist, fusion

--- src/lightning_modules/PFELightningModule.py
+++ src/lightning_modules/PFELightningModule.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator
 from pytorch_metric_learning import distances
 from pytorch_metric_learning.utils.inference import CustomKNN
+import torch.distributions as tdist
 
 from src.lightning_modules.BaseLightningModule import BaseLightningModule
 
@@ -25,6 +26,11 @@ class PFELightningModule(BaseLightningModule):
         # Avoid FAISS error on Ampere GPUs
         knn_func = CustomKNN(distances.LpDistance())
 
+        max_lr = 0.0006
+        self.scheduler.max_lrs = self.scheduler._format_param(
+            "max_lr", optimizer, max_lr
+        )
+
         # Metric calculation
         self.metric_calc = AccuracyCalculator(
             include=("mean_average_precision_at_r", "precision_at_1"),
@@ -35,12 +41,29 @@ class PFELightningModule(BaseLightningModule):
 
     def train_step(self, X, y):
         mu, std = self.forward(X)
-        pdist = torch.distributions.Normal(mu, std)
+        # pdist = torch.distributions.Normal(mu, std)
+        cov = torch.stack([torch.diag(s) for s in (std)])
+        pdist = tdist.MultivariateNormal(mu, cov)
         sample = self.to_device(pdist.rsample())
 
         pairs = self.miner(sample, y)
 
         var = std.square()
+
+        
+        # TODO switch out 128 with batch size and vectorize
+
+        var_inv = (1/var).sum(axis=0).repeat(128,1)
+        var_n = 1/var_inv
+
+        mu_copy = mu.clone()
+        for i in range(128):
+            numerator = var_n[i,:].clone().repeat(128,1)
+            mu_copy[i,:] = numerator.div(var.clone()).mul(mu.clone()).sum(axis=0)
+
+        mu = mu_copy.clone()
+        # var = var_n.clone()
+
         loss = self.loss_fn(embeddings=mu, ref_emb=var, indices_tuple=pairs)
 
         self.metrics.update("train_loss", loss.item())
@@ -50,7 +73,9 @@ class PFELightningModule(BaseLightningModule):
     def val_step(self, X, y):
         mu, std = self.forward(X)
 
-        pdist = torch.distributions.Normal(mu, std)
+        # pdist = torch.distributions.Normal(mu, std)
+        cov = torch.stack([torch.diag(s) for s in (std)])
+        pdist = tdist.MultivariateNormal(mu, cov)
         sample = self.to_device(pdist.rsample())
 
         pairs = self.miner(sample, y)
