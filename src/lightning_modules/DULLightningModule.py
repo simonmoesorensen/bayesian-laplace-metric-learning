🐞 fix(all): Fixes wrong variance in MultivariateNormal covariance matrix and implements it where missing

src/lightning_modules/DULLightningModule.py
@@ -56,10 +56,11 @@ class DULLightningModule(BaseLightningModule):
         self.log(["train_loss", "train_loss_kl", "train_accuracy", "train_map_r"])
 
     def loss_step(self, mu, std, y, step):
-        pdist = dist.Normal(mu, std)
-        samples = pdist.rsample()
-
         variance_dul = std.square()
+        
+        cov = torch.diag_embed(variance_dul)
+        pdist = dist.MultivariateNormal(mu, cov)
+        samples = pdist.rsample()
 
         hard_pairs = self.miner(samples, y)
         loss_backbone = self.loss_fn(samples, y, hard_pairs)
@@ -117,7 +118,8 @@ class DULLightningModule(BaseLightningModule):
         mu_dul, std_dul = self.forward(X)
 
         # Reparameterization trick
-        pdist = dist.Normal(mu_dul, std_dul)
+        cov = torch.diag_embed(std_dul.square())
+        pdist = dist.MultivariateNormal(mu_dul, cov)
         samples = pdist.rsample()
 
         return mu_dul, std_dul, samples

src/lightning_modules/HIBLightningModule.py
@@ -100,7 +100,7 @@ class HIBLightningModule(BaseLightningModule):
         ap, pos, an, neg = ap.view(-1), pos.view(-1), an.view(-1), neg.view(-1)
 
         # Create sample distribution
-        cov = torch.diag_embed(std)
+        cov = torch.diag_embed(std.square())
         pdist = tdist.MultivariateNormal(mu, cov)
 
         # Compare to unit gaussian r(z) ~ N(0, I)

src/lightning_modules/PFELightningModule.py
@@ -35,7 +35,8 @@ class PFELightningModule(BaseLightningModule):
 
     def train_step(self, X, y):
         mu, std = self.forward(X)
-        pdist = torch.distributions.Normal(mu, std)
+        cov = torch.diag_embed(std.square())
+        pdist = torch.distributions.MultivariateNormal(mu, cov)
         sample = self.to_device(pdist.rsample())
 
         pairs = self.miner(sample, y)
@@ -50,7 +51,8 @@ class PFELightningModule(BaseLightningModule):
     def val_step(self, X, y):
         mu, std = self.forward(X)
 
-        pdist = torch.distributions.Normal(mu, std)
+        cov = torch.diag_embed(std.square())
+        pdist = torch.distributions.MultivariateNormal(mu, cov)
         sample = self.to_device(pdist.rsample())
 
         pairs = self.miner(sample, y)
@@ -64,7 +66,8 @@ class PFELightningModule(BaseLightningModule):
     def test_step(self, X, y):
         mu, std = self.forward(X)
 
-        pdist = torch.distributions.Normal(mu, std)
+        cov = torch.diag_embed(std.square())
+        pdist = torch.distributions.MultivariateNormal(mu, cov)
         sample = self.to_device(pdist.rsample())
 
         return mu, std, sample