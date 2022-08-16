6d74784466f5a52d897d2c6a357be12ae6cd71e4
simonmoesorensen <moe.simon@gmail.com>
2022-08-16 14:14:45 +0200

    ✨ feat(datamodules): Set svhn as OOD for cifar

--- src/data_modules/CIFAR10DataModule.py
+++ src/data_modules/CIFAR10DataModule.py
@@ -28,50 +28,47 @@ class CIFAR10DataModule(BaseDataModule):
                     (0.49139968, 0.48215841, 0.44653091),
                     (0.24703223, 0.24348513, 0.26158784),
                 ),
-                transforms.RandomCrop((32, 32), padding=4),
-                transforms.RandomHorizontalFlip(0.5),
             ]
         )
 
     def prepare_data(self):
         super().prepare_data()
-        d.DTD(self.data_dir, split="test", download=True)
+        d.SVHN(self.data_dir, split="test", download=True)
 
     def setup(self, val_split=0.2, shuffle=True):
         super().setup(val_split, shuffle)
 
-        # Set DTD (Describable Textures Dataset) as OOD dataset
+        # Set SVHN (Street View House Numbers) as OOD dataset
         ood_transforms = transforms.Compose(
             [
                 transforms.ToTensor(),
                 # Found using self._compute_mean_and_std()
                 transforms.Normalize(
-                    (0.5287, 0.4742, 0.4236), (0.2588, 0.2499, 0.2553)
-                ),
-                transforms.RandomCrop((32, 32), padding=4),
-                transforms.RandomHorizontalFlip(0.5),
-            ]
-        )
-
-        self.dataset_ood = d.DTD(self.data_dir, split="test", transform=ood_transforms)
-
-        # OOD noise
-        ood_noise_transforms = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize(
-                    (0.49139968, 0.48215841, 0.44653091),
-                    (0.24703223, 0.24348513, 0.26158784),
-                ),
-                transforms.RandomCrop((32, 32), padding=4),
-                transforms.RandomHorizontalFlip(0.5),
-                transforms.GaussianBlur(
-                    kernel_size=7,
-                    sigma=3,
+                    (0.4376821, 0.4437697, 0.47280442),
+                    (0.19803012, 0.20101562, 0.19703614),
                 ),
             ]
         )
 
-        self.dataset_ood = d.CIFAR10(
-            self.data_dir, train=False, transform=ood_noise_transforms
-        )
+        self.dataset_ood = d.SVHN(self.data_dir, split="test", transform=ood_transforms)
+
+        # # OOD noise
+        # ood_noise_transforms = transforms.Compose(
+        #     [
+        #         transforms.ToTensor(),
+        #         transforms.Normalize(
+        #             (0.49139968, 0.48215841, 0.44653091),
+        #             (0.24703223, 0.24348513, 0.26158784),
+        #         ),
+        #         transforms.RandomCrop((32, 32), padding=4),
+        #         transforms.RandomHorizontalFlip(0.5),
+        #         transforms.GaussianBlur(
+        #             kernel_size=7,
+        #             sigma=3,
+        #         ),
+        #     ]
+        # )
+
+        # self.dataset_ood = d.CIFAR10(
+        #     self.data_dir, train=False, transform=ood_noise_transforms
+        # )
