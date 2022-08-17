5790131a9ccb7efce06d4119299ce0868b1fea75
simonmoesorensen <moe.simon@gmail.com>
2022-08-17 14:46:01 +0200

    ✨ feat(backbone): Change default seed to 42

--- src/baselines/Backbone/config.py
+++ src/baselines/Backbone/config.py
@@ -20,7 +20,7 @@ def parse_args():
     parser.add_argument("--name", type=str, default="MNIST")
 
     # ----- random seed for reproducing
-    parser.add_argument("--random_seed", type=int, default=6666)
+    parser.add_argument("--random_seed", type=int, default=42)
 
     # ----- directory (train & test)
     parser.add_argument("--dataset", type=str)
