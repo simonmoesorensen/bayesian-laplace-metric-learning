04199d6ea3f313b2170502c52ed584bc4dd165d1
simonmoesorensen <moe.simon@gmail.com>
2022-09-06 15:37:08 +0200

    🐞 fix(experiments): Fixes laplace missing configuration

--- experiments/submit_experiments_laplace.py
+++ experiments/submit_experiments_laplace.py
@@ -1,15 +1,25 @@
-from config import FashionMNISTConfigLaplace, CIFAR10ConfigLaplace, template_text_laplace
+from config import (
+    FashionMNISTConfigLaplace,
+    CIFAR10ConfigLaplace,
+    template_text_laplace,
+)
 import subprocess
 from pathlib import Path
 
 root = Path(__file__).parent.parent
 
-# Submit FashionMNIST experiments
 for config in [FashionMNISTConfigLaplace, CIFAR10ConfigLaplace]:
     for latent_dim in config.latent_dims:
         for model in config.models:
+            if model == "PostHoc":
+                train_script = "train_post_hoc"
+            elif model == "Online":
+                train_script = "train_online"
+            else:
+                raise ValueError(f"Unknown model {model}")
+
             for hessian in config.hessians:
-                for seed in config.seeds:  
+                for seed in config.seeds:
                     additional_args_seed = f"--random_seed {seed}"
 
                     batch_size = 16
@@ -28,23 +38,25 @@ for config in [FashionMNISTConfigLaplace, CIFAR10ConfigLaplace]:
                             "batch_size": batch_size,
                             "latent_dim": latent_dim,
                             "num_epoch": config.num_epoch,
+                            "gpu_queue": config.gpu_queue,
                             "gpu_mem": config.gpu_mem,
                             "hessian": hessian,
-                            "additional_args": additional_args_seed
+                            "additional_args": additional_args_seed,
+                            "train_script": train_script,
                         }
                     )
 
-                    submit_file = log_dir / 'script.sh'
+                    submit_file = log_dir / "script.sh"
 
-                    with open(submit_file, 'w') as f:
+                    with open(submit_file, "w") as f:
                         f.write(submit_script)
 
-                    print('Submitting job:', submit_file)
+                    print("Submitting job:", submit_file)
                     # Execute code in terminal
                     normal = subprocess.run(
                         f"bsub < {submit_file}",
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                         check=True,
-                        shell=True
+                        shell=True,
                     )
