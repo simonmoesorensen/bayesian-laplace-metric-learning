60436b18861adbc3ba8488e5173f6af5926e8abc
simonmoesorensen <moe.simon@gmail.com>
2022-09-06 15:26:25 +0200

    🖥️ build(scripts): Update pfe script

--- scripts/PFE/CIFAR10/train.sh
+++ scripts/PFE/CIFAR10/train.sh
@@ -9,10 +9,11 @@ python3 -m debugpy --listen $node_ip:1332 ./src/baselines/PFE/train.py \
     --dataset CIFAR10 \
     --name train_script \
     --batch_size 256 \
-    --embedding_size 16 \
-    --num_epoch 20 \
-    --save_freq 2 \
+    --embedding_size 64 \
+    --num_epoch 10 \
+    --save_freq 11 \
     --gpu_id 0 1\
     --num_workers 8 \
     --shuffle \
+    --random_seed 42 \
     --to_visualize
