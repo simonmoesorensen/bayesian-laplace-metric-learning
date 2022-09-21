58f6549101d2468cf45abe1f5bc662da5a70c326
Frederik Rahbaek Warburg <frewar1905@gmail.com>
2022-09-21 10:15:24 +0200

    call the correct scripts

--- scripts/PostHoc/FashionMNIST/train.sh
+++ scripts/PostHoc/FashionMNIST/train.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python3 -m debugpy --listen $node_ip:1332 ./src/baselines/Laplace_posthoc/train.py \
     --dataset FashionMNIST \
-    --name train_script_full_nneg_0 \
+    --name train_script_full_nneg_5 \
     --model_path src/baselines/PFE/pretrained/FashionMNIST/latentdim_32_seed_42.pth \
     --hessian full \
     --embedding_size 32 \
